b4b4afb37c2c58b383f1929eb9c9c8655817fd07
Eray Ateş <eray.aates@gmail.com>
2025-09-01 15:14:37 +0300

    Folder structure created

---
+++ src/components/TittleCards/TittleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'TittleCards.css'
+const TittleCards = () => {
+  return (
+    <div className='tittlecards'>
+      
+    </div>
+  )
+}
+
+export default TittleCards
