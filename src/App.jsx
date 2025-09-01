c64c3d2ed89604e24f422c6ba820671c8cf7e4eb
Eray Ateş <eray.aates@gmail.com>
2025-09-02 00:36:45 +0300

    Login pages done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import { Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
+      
     </div>
   )
 }
