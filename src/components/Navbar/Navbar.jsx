b0c2fa7005b4365176bbecd1082d24a65a388bf3
Eray Ateş <eray.aates@gmail.com>
2025-09-05 19:21:21 +0300

    FireBase authentication service created. All api keys are store in the .env file. Login and register screen have a meaning right now. User can create a profile and then watch trailers.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
 import profile_icon from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
+import { logout } from '../../firebase'
 const Navbar = () => {
+
+  const navRef = useRef();
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+    if (window.scrollY >= 80) {
+      navRef.current.classList.add('nav-dark');
+    } else {
+      navRef.current.classList.remove('nav-dark');
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
   return (
-    <div className='navbar'>
+    <div ref={navRef} className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -28,7 +49,7 @@ const Navbar = () => {
           <img src={profile_icon} alt=""  className='profile'/>
           <img src={caret_icon} alt=""  className='profile'/>
           <div className="dropdown">
-            <p>Sign Out</p>
+            <p onClick={()=>{logout()}}>Sign Out</p>
           </div>
         </div>
       </div>
